766466632a3dda78b21ac2081db313e4f0094723
unknown <arun>
2019-07-21 22:54:54 +0530

    added algo

--- Nodejs/Sorting/SortObjectsOnAttributes/test.js
+++ Nodejs/Sorting/SortObjectsOnAttributes/test.js
@@ -1,8 +1,36 @@
 const Assert = require('assert');
 const code = require('./code');
+
+var inputData = [
+    { day: 12, month: 5, year: 1200 },
+    { day: 29, month: 9, year: 2018 },
+    { day: 31, month: 10, year: 2010 },
+    { day: 12, month: 4, year: 2000 },
+    { day: 01, month: 12, year: 1999 },
+    { day: 12, month: 5, year: 1999 },
+    { day: 12, month: 4, year: 2019 },
+    { day: 31, month: 11, year: 2018 },
+    { day: 12, month: 6, year: 1999 },
+    { day: 11, month: 4, year: 1991 }
+];
+var outputData = [
+    { day: 12, month: 4, year: 2019 },
+    { day: 31, month: 11, year: 2018 },
+    { day: 29, month: 9, year: 2018 },
+    { day: 31, month: 10, year: 2010 },
+    { day: 12, month: 4, year: 2000 },
+    { day: 01, month: 12, year: 1999 },
+    { day: 12, month: 6, year: 1999 },
+    { day: 12, month: 5, year: 1999 },
+    { day: 11, month: 4, year: 1991 },
+    { day: 12, month: 5, year: 1200 }
+];
+
 try {
-    Assert.deepEqual(code.algorithm(), []);
+    var algoOutput = code.algorithm(inputData);
+    var t = JSON.stringify(algoOutput);
+    Assert.deepEqual(algoOutput, outputData);
+    console.log('Output verified');
 } catch (ex) {
     console.log('Error in code');
 }
-console.log('Output verified');
