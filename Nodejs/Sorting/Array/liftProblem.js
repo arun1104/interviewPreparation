8367e4a73e4d990e409ab78f33237902446a790a
arun1104 <arunrajeevan1104@gmail.com>
2019-08-05 16:10:06 +0530

    Update liftProblem.js

--- Nodejs/Sorting/Array/liftProblem.js
+++ Nodejs/Sorting/Array/liftProblem.js
@@ -8,22 +8,18 @@ You are provided arrays A and B, and values M, X and Y:
         M (number of floors in the building),
         X (maximum capacity of lift),
         Y (maximum weight limit on lift)
-
     Given that people must be served in the order they arrive,
     Write a function to calculate the number of floors the lift must stop at and come back
     to the ground floor in order to service all of the people.
-
     eg. A = [40, 40, 100, 60], B = [3, 3, 2, 2], M = 3, X = 3, Y = 200
     1) Lift will take person 1, 2, 3 to floor 3 and floor 2 => 2 floors
     2) Lift will return to ground floor => 1 floor
     3) Lift will take person 4 to floor 2 => 1 floor
     4) Lift will return to ground floor => 1 floor
     Total floors = 5
-
 Your answer must be within:
     Worst time complexity = O(N)
     Auxillary space complexity = O(N)
-
 Please provide your answer below
 */
 module.exports = function(A, B, M, X, Y) {
@@ -34,6 +30,7 @@ module.exports = function(A, B, M, X, Y) {
     const maxPeopleWeight = Y;
     const floorsToHalt = new Set();
     let peopleOnLift = [];
+    let currentWeight = 0;
     let result = 0;
     //Given that people must be served in the order they arrive
     /* Because of the above mentioned statement, it can be assumed that if a person with less weight 
@@ -41,14 +38,18 @@ module.exports = function(A, B, M, X, Y) {
     */
     for (let index = 0; index < weightsOfPeople.length; index++) {
         peopleOnLift.push(weightsOfPeople[index]);
+        currentWeight+=weightsOfPeople[index];
         let ifLiftFull = checkIfLiftFull(peopleOnLift, maxPeople, maxPeopleWeight);
         if (ifLiftFull) {
             //lift will go up
             peopleOnLift.pop();
+            currentWeight-=weightsOfPeople[index];
             result += floorsToHalt.size + 1;
             peopleOnLift = [];
+            currentWeight=0;
             floorsToHalt.clear();
             peopleOnLift.push(weightsOfPeople[index]);
+            currentWeight=weightsOfPeople[index];
             floorsToHalt.add(floorsToVisit[index]);
         } else {
             floorsToHalt.add(floorsToVisit[index]);
