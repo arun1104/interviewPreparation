72b13418ba05a85202605f0cb0dc4bf5121e3abc
arun1104 <arunrajeevan1104@gmail.com>
2019-08-05 16:08:44 +0530

    Update test.js

--- Nodejs/Sorting/Array/test.js
+++ Nodejs/Sorting/Array/test.js
@@ -8,7 +8,9 @@ const expect = chai.expect;
 
 const testCases = [
     { A: [40, 40, 100, 60], B: [3, 3, 2, 2], M: 3, X: 3, Y: 200, totalFloors: 5 },
-    { A: [40, 40, 100, 60, 20], B: [5, 4, 2, 1, 5], M: 5, X: 3, Y: 200, totalFloors: 7 }
+    { A: [40, 40, 100, 60, 20], B: [5, 4, 2, 1, 5], M: 5, X: 3, Y: 200, totalFloors: 7 },
+    { A: [200, 200, 200, 200, 200], B: [5, 4, 2, 1, 5], M: 5, X: 3, Y: 200, totalFloors: 7 },
+    { A: [40, 40, 100, 60], B: [3, 3, 2, 2], M: 3, X: 1, Y: 200, totalFloors: 5 },
 ];
 
 describe('Testing src/challenge.js', function() {
